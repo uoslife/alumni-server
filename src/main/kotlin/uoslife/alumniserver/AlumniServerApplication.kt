6e5bcfc10d316ebafce839e5ab6a419fea0a4412
esfuerzo <dkdldrha@naver.com>
2024-10-16 20:43:16 +0900

    init: 스프링부트 초기세팅 #1

---
+++ src/main/kotlin/uoslife/alumniserver/AlumniServerApplication.kt
@@ -0,0 +1,11 @@
+package uoslife.alumniserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AlumniServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<AlumniServerApplication>(*args)
+}
---
+++ src/test/kotlin/uoslife/alumniserver/AlumniServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package uoslife.alumniserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AlumniServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
